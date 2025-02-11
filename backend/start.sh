b490e62c8a603df1818c766ec4fd287dc8ea8df3
Gungrave <jorgefranciscocz@gmail.com>
2025-02-10 19:31:22 -0600

    8

--- backend/start.sh
+++ backend/start.sh
@@ -1,7 +1,2 @@
-#!/bin/bash
-
-# Arrancar el servidor FastAPI
-uvicorn backend.main:app --host 0.0.0.0 --port $PORT &
-
-# Arrancar el servidor estático para React
-npx serve -s frontend/build
+chmod +x start.sh
+uvicorn main:app --host 0.0.0.0 --port $PORT
